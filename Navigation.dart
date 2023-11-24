6fa20b5a08c1cdd9bead5e0371371e92674e3b19
AadityaaGupta <151832733+AadityaaGupta@users.noreply.github.com>
2023-11-24 08:37:32 +0530

    Add files via upload

--- Navigation.dart
+++ Navigation.dart
@@ -9,73 +9,60 @@ class Navigation extends StatefulWidget {
 
 class _NavigationState extends State<Navigation> {
 
-TextEditingController _field=TextEditingController();
-TextEditingController _check=TextEditingController();
+//TextEditingController _field=TextEditingController();
+//TextEditingController _check=TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     var _mediaQuerry= MediaQuery.of(context);
     return Column(
+            
+
       children: [
 
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-
-                Expanded(
-                  child: Container(
-                   width: _mediaQuerry.size.width*0.8, 
-                  height: _mediaQuerry.size.height*0.1,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      width: 2.0,
-                      color: Colors.black,
-                      
-                    )
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+          
+              Expanded(
+                child: Container(
+                 
+                        margin: EdgeInsets.all(5.0),
+                child: TextField(
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w900,
                   ),
-                  child: TextField(
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w900,
-                    ),
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50.0),
-                        ),
-                        hintText: ' Search ',
-                        suffixIcon: const Icon(
-                          Icons.search,
-                          size: 30.0,
-                        )),
-                  ),
-                          ),
-                ),
-
-                Expanded(
-                  child: Container(
-                   height: _mediaQuerry.size.width*0.01,
-                  width:_mediaQuerry.size.height*0.01,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          width: 2.0,
-                          color: Colors.black,
-                          
-                        )
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50.0),
                       ),
-                        child: ElevatedButton(
-                          style: ButtonStyle(
-                          
-                            
-                          ),
-                          onPressed: (){}, child: Text('pick')),
-                  ),
+                      hintText: ' Search ',
+                      suffixIcon: const Icon(
+                        Icons.search,
+                        size: 30.0,
+                      )),
                 ),
-
-              ],
-            ),
+                        ),
+              ),
+          
+              Container(
+                margin: EdgeInsets.all(15.5),
+                    width: 100.0,
+                  
+                    child: ElevatedButton(
+                     style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange.shade100,
+                      
+                     ), 
+                      onPressed: (){}, child: Text('pick',style: TextStyle(
+                        color: Colors.blueGrey.shade600,
+                      ),)),
+              ),
+          
+            ],
           ),
 
 
